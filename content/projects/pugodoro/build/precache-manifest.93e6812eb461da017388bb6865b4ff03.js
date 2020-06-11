5a0229d475aaf9b1a5244ab2a5b7fcc4f9ad6d91
Arie <aoldman@deputy.com>
2020-06-11 23:12:10 +1000

    add pugodoro

---
+++ content/projects/pugodoro/build/precache-manifest.93e6812eb461da017388bb6865b4ff03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff39f832de881c1bab26e3ca8d25203e",
+    "url": "/projects/pugodoro/build/index.html"
+  },
+  {
+    "revision": "d900c3cf8dfd1dc0b844",
+    "url": "/projects/pugodoro/build/static/css/2.c4f291ab.chunk.css"
+  },
+  {
+    "revision": "f21694678d19d9f84be1",
+    "url": "/projects/pugodoro/build/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "d900c3cf8dfd1dc0b844",
+    "url": "/projects/pugodoro/build/static/js/2.61a326f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projects/pugodoro/build/static/js/2.61a326f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f21694678d19d9f84be1",
+    "url": "/projects/pugodoro/build/static/js/main.55172410.chunk.js"
+  },
+  {
+    "revision": "e961470616f784ae0fbc",
+    "url": "/projects/pugodoro/build/static/js/runtime-main.91d7d9d0.js"
+  }
+]);
