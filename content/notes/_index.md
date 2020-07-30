b790a832e5cc8e5f55c1aceb36926e9555abfdc9
Arie <aoldman@deputy.com>
2020-07-30 15:56:42 +1000

    New Project - change rendering for images. Add deputy environment variables

--- content/notes/_index.md
+++ content/notes/_index.md
@@ -5,7 +5,7 @@ type: "section"
 
 {{< callout 
     "danger" 
-    `This is a collection of study notes. 
+    `This is a collection of study notes and other docs. 
     I like to keep them here because they get formatted nicely and
     I can tag them to organise them. Also I could send them
     to people. This section should not have any backlinks to it. 
--- less-coffee-lambda-at-edge/src/index.spec.js
+++ less-coffee-lambda-at-edge/src/index.spec.js
@@ -1,58 +1,58 @@
-const index = require('./index');
+const {addTrailingSlash, resolveIndexPath, rewriteURI} = require('./index');
 const Url = require('url-parse');
 
 test('Adds trailing slash correctly on URIs', () => {
-    expect(index.addTrailingSlash(new Url('')).pathname)
+    expect(addTrailingSlash(new Url('')).pathname)
         .toBe('/');
-    expect(index.addTrailingSlash(new Url('/')).pathname)
+    expect(addTrailingSlash(new Url('/')).pathname)
         .toBe('/');
-    expect(index.addTrailingSlash(new Url('/posts')).pathname)
+    expect(addTrailingSlash(new Url('/posts')).pathname)
         .toBe('/posts/');
-    expect(index.addTrailingSlash(new Url('/posts/')).pathname)
+    expect(addTrailingSlash(new Url('/posts/')).pathname)
         .toBe('/posts/');
-    expect(index.addTrailingSlash(new Url('/posts/index.html')).pathname)
+    expect(addTrailingSlash(new Url('/posts/index.html')).pathname)
         .toBe('/posts/index.html');
 });
 
 test('Adds index.html on URIs which have trailing slash', () => {
-    expect(index.resolveIndexPath(new Url('/')).pathname)
+    expect(resolveIndexPath(new Url('/')).pathname)
         .toBe('/index.html');
-    expect(index.resolveIndexPath(new Url('/posts/')).pathname)
+    expect(resolveIndexPath(new Url('/posts/')).pathname)
         .toBe('/posts/index.html');
-    expect(index.resolveIndexPath(new Url('/index.html')).pathname)
+    expect(resolveIndexPath(new Url('/index.html')).pathname)
         .toBe('/index.html');
-    expect(index.resolveIndexPath(new Url('/posts')).pathname)
+    expect(resolveIndexPath(new Url('/posts')).pathname)
         .toBe('/posts');
-    expect(index.resolveIndexPath(new Url('')).pathname)
+    expect(resolveIndexPath(new Url('')).pathname)
         .toBe('/index.html');
 });
 
 test('Resolve URLs using multiple rewrite functions in series', () => {
-    expect(index.rewriteURI(''))
+    expect(rewriteURI(''))
         .toBe('/index.html');
-    expect(index.rewriteURI('/'))
+    expect(rewriteURI('/'))
         .toBe('/index.html');
-    expect(index.rewriteURI('/index.html'))
+    expect(rewriteURI('/index.html'))
         .toBe('/index.html');
-    expect(index.rewriteURI('/posts'))
+    expect(rewriteURI('/posts'))
         .toBe('/posts/index.html');
-    expect(index.rewriteURI('/posts/'))
+    expect(rewriteURI('/posts/'))
         .toBe('/posts/index.html');
-    expect(index.rewriteURI('/posts/index.html'))
+    expect(rewriteURI('/posts/index.html'))
         .toBe('/posts/index.html');
-    expect(index.rewriteURI('/posts/pug-go-doro'))
+    expect(rewriteURI('/posts/pug-go-doro'))
         .toBe('/posts/pug-go-doro/index.html');
-    expect(index.rewriteURI('/posts/pug-go-doro/'))
+    expect(rewriteURI('/posts/pug-go-doro/'))
         .toBe('/posts/pug-go-doro/index.html');
-    expect(index.rewriteURI('/posts/pug-go-doro/index.html'))
+    expect(rewriteURI('/posts/pug-go-doro/index.html'))
         .toBe('/posts/pug-go-doro/index.html');
-    expect(index.rewriteURI('/posts/pug-go-doro/help.png'))
+    expect(rewriteURI('/posts/pug-go-doro/help.png'))
         .toBe('/posts/pug-go-doro/help.png');
-    expect(index.rewriteURI('/posts/pug-go-doro/index.xml'))
+    expect(rewriteURI('/posts/pug-go-doro/index.xml'))
         .toBe('/posts/pug-go-doro/index.xml');
-    expect(index.rewriteURI('/posts/pug-go-doro'))
+    expect(rewriteURI('/posts/pug-go-doro'))
         .toBe('/posts/pug-go-doro/index.html');
-    expect(index.rewriteURI('/posts/pug-go-doro'))
+    expect(rewriteURI('/posts/pug-go-doro'))
         .toBe('/posts/pug-go-doro/index.html');
 });
 
