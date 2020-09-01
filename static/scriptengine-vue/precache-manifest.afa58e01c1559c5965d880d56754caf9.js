d27a1cafa680996c27f6974ca5dee974d8c1d4af
Arie <aoldman@deputy.com>
2020-09-01 18:01:36 +1000

    add new tools to scriptengine editor, make PWA

---
+++ static/scriptengine-vue/precache-manifest.afa58e01c1559c5965d880d56754caf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6056c3706daf0d3f7fc7",
+    "url": "/scriptengine-vue/css/app.64e50988.css"
+  },
+  {
+    "revision": "47a417fd3f21b8956dd8",
+    "url": "/scriptengine-vue/css/chunk-vendors.9a8d9793.css"
+  },
+  {
+    "revision": "8f89d3b89cab54fb428076a42a246dcf",
+    "url": "/scriptengine-vue/index.html"
+  },
+  {
+    "revision": "f34f962696cfbc88cd8b",
+    "url": "/scriptengine-vue/js/about.5838fbf8.js"
+  },
+  {
+    "revision": "6056c3706daf0d3f7fc7",
+    "url": "/scriptengine-vue/js/app.583b90f1.js"
+  },
+  {
+    "revision": "47a417fd3f21b8956dd8",
+    "url": "/scriptengine-vue/js/chunk-vendors.8357ea9d.js"
+  },
+  {
+    "revision": "4ce810ed567eb36bfef5fa68195f5922",
+    "url": "/scriptengine-vue/manifest.json"
+  }
+]);
